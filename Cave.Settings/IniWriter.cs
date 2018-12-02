626b9b13a629a82b6b2dc2bb30eef1b5089e69d2
Dingsd4 <39777727+Dingsd4@users.noreply.github.com>
2018-12-02 17:32:31 +0100

    Moved File* and RootLocation to Cave.FileSystem package

--- Cave.Settings/IniWriter.cs
+++ Cave.Settings/IniWriter.cs
@@ -1,49 +1,3 @@
-#region CopyRight 2018
-/*
-    Copyright (c) 2003-2018 Andreas Rohleder (andreas@rohleder.cc)
-    All rights reserved
-*/
-#endregion
-#region License LGPL-3
-/*
-    This program/library/sourcecode is free software; you can redistribute it
-    and/or modify it under the terms of the GNU Lesser General Public License
-    version 3 as published by the Free Software Foundation subsequent called
-    the License.
-
-    You may not use this program/library/sourcecode except in compliance
-    with the License. The License is included in the LICENSE file
-    found at the installation directory or the distribution package.
-
-    Permission is hereby granted, free of charge, to any person obtaining
-    a copy of this software and associated documentation files (the
-    "Software"), to deal in the Software without restriction, including
-    without limitation the rights to use, copy, modify, merge, publish,
-    distribute, sublicense, and/or sell copies of the Software, and to
-    permit persons to whom the Software is furnished to do so, subject to
-    the following conditions:
-
-    The above copyright notice and this permission notice shall be included
-    in all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-#endregion
-#region Authors & Contributors
-/*
-   Author:
-     Andreas Rohleder <andreas@rohleder.cc>
-
-   Contributors:
- */
-#endregion Authors & Contributors
-
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -54,15 +8,15 @@ using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
 using Cave.Compression;
+using Cave.FileSystem;
 using Cave.IO;
-using Cave.Text;
 
 namespace Cave
 {
-	/// <summary>
-	/// Provides a fast and simple ini writer class.
-	/// </summary>
-	[DebuggerDisplay("{Name}")]
+    /// <summary>
+    /// Provides a fast and simple ini writer class.
+    /// </summary>
+    [DebuggerDisplay("{Name}")]
     public class IniWriter
     {
         #region static constructors
@@ -118,7 +72,7 @@ namespace Cave
         /// <returns></returns>
         public static IniWriter FromStream(string name, Stream stream, int count, IniProperties properties = default(IniProperties))
         {
-            var data = stream.ReadBlock(count);
+            byte[] data = stream.ReadBlock(count);
             return Parse(name, data, properties);
         }
 
@@ -143,7 +97,7 @@ namespace Cave
         /// <returns></returns>
         public static IniWriter FromLocation(RootLocation root, IniProperties properties = default(IniProperties))
         {
-            var fileLocation = new FileLocation(root: root, extension: Ini.PlatformExtension);
+            FileLocation fileLocation = new FileLocation(root: root, extension: Ini.PlatformExtension);
             return FromLocation(fileLocation, properties);
         }
 
@@ -171,16 +125,16 @@ namespace Cave
             Load(reader);
         }
 
-		/// <summary>Initializes a new instance of the <see cref="IniWriter"/> class.</summary>
-		public IniWriter()
-		{
-			Properties = IniProperties.Default;
-		}
+        /// <summary>Initializes a new instance of the <see cref="IniWriter"/> class.</summary>
+        public IniWriter()
+        {
+            Properties = IniProperties.Default;
+        }
 
-		/// <summary>
-		/// Creates a new empty writer instance
-		/// </summary>
-		public IniWriter(string fileName, IniProperties properties)
+        /// <summary>
+        /// Creates a new empty writer instance
+        /// </summary>
+        public IniWriter(string fileName, IniProperties properties)
         {
             Properties = properties.Valid ? properties : IniProperties.Default;
             Name = fileName;
@@ -281,7 +235,7 @@ namespace Cave
             }
 
             List<string> strings = new List<string>();
-            foreach(object value in values)
+            foreach (object value in values)
             {
                 strings.Add(value.ToString());
             }
@@ -307,7 +261,7 @@ namespace Cave
             }
 
             List<string> result = new List<string>();
-			result.AddRange(lines);
+            result.AddRange(lines);
             m_Data[section] = result;
         }
 
@@ -360,24 +314,24 @@ namespace Cave
             m_Data[section] = sections;
         }
 
-		/// <summary>
-		/// Writes a setting to the ini file (replacing a present one) 
-		/// </summary>
-		/// <param name="section">Name of the section</param>
-		/// <param name="name">Name of the setting</param>
-		/// <param name="value">Value of the setting</param>
-		public void WriteSetting(string section, string name, object value)
-		{
-			WriteSetting(section, name, StringExtensions.ToString(value, Properties.Culture));
-		}
+        /// <summary>
+        /// Writes a setting to the ini file (replacing a present one) 
+        /// </summary>
+        /// <param name="section">Name of the section</param>
+        /// <param name="name">Name of the setting</param>
+        /// <param name="value">Value of the setting</param>
+        public void WriteSetting(string section, string name, object value)
+        {
+            WriteSetting(section, name, StringExtensions.ToString(value, Properties.Culture));
+        }
 
-		/// <summary>
-		/// Writes a setting to the ini tile (replacing a present one) 
-		/// </summary>
-		/// <param name="section">Name of the section</param>
-		/// <param name="name">Name of the setting</param>
-		/// <param name="value">Value of the setting</param>
-		public void WriteSetting(string section, string name, string value)
+        /// <summary>
+        /// Writes a setting to the ini tile (replacing a present one) 
+        /// </summary>
+        /// <param name="section">Name of the section</param>
+        /// <param name="name">Name of the setting</param>
+        /// <param name="value">Value of the setting</param>
+        public void WriteSetting(string section, string name, string value)
         {
             if (section == null)
             {
@@ -402,25 +356,25 @@ namespace Cave
             List<string> result;
             if (m_Data.ContainsKey(section))
             {
-				result = m_Data[section];
+                result = m_Data[section];
             }
             else
             {
-				result = new List<string>();
+                result = new List<string>();
                 m_Data[section] = result;
             }
-			//try to replace first
-			for (int i = 0; i < result.Count; i++)
-			{
-				string setting = result[i].BeforeFirst('=').Trim();
-				if (string.Equals(setting, name.Trim(), StringComparison.OrdinalIgnoreCase))
-				{
-					result[i] = name + "=" + value;
-					return;
-				}
-			}
-			//add new one
-			result.Add(name + "=" + value);
+            //try to replace first
+            for (int i = 0; i < result.Count; i++)
+            {
+                string setting = result[i].BeforeFirst('=').Trim();
+                if (string.Equals(setting, name.Trim(), StringComparison.OrdinalIgnoreCase))
+                {
+                    result[i] = name + "=" + value;
+                    return;
+                }
+            }
+            //add new one
+            result.Add(name + "=" + value);
         }
 
         /// <summary>
@@ -429,7 +383,7 @@ namespace Cave
         /// <param name="fileName">The fileName to write to</param>
         public void Save(string fileName = null)
         {
-			if (fileName == null)
+            if (fileName == null)
             {
                 fileName = Name;
             }
