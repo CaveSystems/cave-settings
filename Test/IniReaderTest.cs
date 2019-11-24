0a7e283305c1eeb6e720788da499d09601509c63
Andreas Rohleder <andreas.rohleder@daimler.com>
2019-11-24 19:36:11 +0100

    Added some test cases

---
+++ Test/IniReaderTest.cs
@@ -0,0 +1,75 @@
+﻿using Cave;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [TestFixture]
+    public class IniReaderTest
+    {
+        readonly CultureInfo[] allCultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+
+        [Test]
+        public void IniReaderTest1()
+        {
+            var temp = Path.GetTempFileName();
+            foreach (var culture in allCultures)
+            {
+                Console.WriteLine($"IniReaderTest.cs: info TI0002: Test {culture}");
+                var settings = new SettingsStructFields[100];
+                var properties = IniProperties.Default;
+                properties.Culture = culture;
+                var writer = new IniWriter(temp, properties);
+
+                for (int i = 0; i < settings.Length; i++)
+                {
+                    var setting = SettingsStructFields.Random();
+                    settings[i] = setting;
+                    writer.WriteFields($"Section {i}", setting);
+                }                
+                writer.Save(temp);
+
+                TestReader(writer.ToSettings(), settings);
+                TestReader(IniReader.FromFile(temp, properties), settings);
+            }
+        }
+
+        private void TestReader(ISettings reader, SettingsStructFields[] settings)
+        {
+            var fields1 = typeof(SettingsStructFields).GetFields();
+            var fields2 = typeof(SettingsObjectFields).GetFields();
+            var fields3 = typeof(SettingsStructProperties).GetProperties();
+            var fields4 = typeof(SettingsObjectProperties).GetProperties();
+            for (int i = 0; i < settings.Length; i++)
+            {
+                var settings1 = reader.ReadStructFields<SettingsStructFields>($"Section {i}");
+                var settings2 = reader.ReadObjectFields<SettingsObjectFields>($"Section {i}");
+                var settings3 = reader.ReadStructProperties<SettingsStructProperties>($"Section {i}");
+                var settings4 = reader.ReadObjectProperties<SettingsObjectProperties>($"Section {i}");
+                
+                for(int n = 0; n < fields1.Length; n++)
+                {
+                    var original = fields1[n].GetValue(settings[i]);
+                    var value1 = fields1[n].GetValue(settings1);
+                    var value2 = fields2[n].GetValue(settings2);
+                    var value3 = fields3[n].GetValue(settings3);
+                    var value4 = fields4[n].GetValue(settings4);
+                    if (!Equals(original , value1))
+                    {
+
+                    }
+                    Assert.AreEqual(original, value1);
+                    Assert.AreEqual(original, value2);
+                    Assert.AreEqual(original, value3);
+                    Assert.AreEqual(original, value4);
+                }
+            }
+        }
+    }
+}
---
+++ Test/SettingsStructFields.cs
@@ -0,0 +1,80 @@
+﻿using Cave;
+using System;
+using System.Text;
+
+namespace Test
+{
+    public struct SettingsStructFields
+    {
+        public string SampleString;
+        public sbyte SampleInt8;
+        public short SampleInt16;
+        public int SampleInt32;
+        public long SampleInt64;
+        public byte SampleUInt8;
+        public ushort SampleUInt16;
+        public uint SampleUInt32;
+        public ulong SampleUInt64;
+        public float SampleFloat;
+        public double SampleDouble;
+        public decimal SampleDecimal;
+        public bool SampleBool;
+        public DateTime SampleDateTime;
+        public TimeSpan SampleTimeSpan;
+        public SettingEnum SampleEnum;
+        public SettingFlagEnum SampleFlagEnum;
+        public uint? SampleNullableUInt32;
+
+        static readonly Random random = new Random(Environment.TickCount);
+
+        public static SettingsStructFields Random()
+        {
+            var len = random.Next(0, 128);
+            char[] str;
+            if (random.Next(1, 100) < 51)
+            {
+                byte[] buf = new byte[len * 2];
+                random.NextBytes(buf);
+                str = Encoding.Unicode.GetString(buf).ToCharArray();
+            }
+            else
+            {
+                byte[] buf = new byte[len];
+                for (int i = 0; i < len; i++)
+                {
+                    if (buf[i] < 16) buf[i] = (byte)' ';
+                }
+                random.NextBytes(buf);
+                str = Encoding.GetEncoding(1250 + random.Next(0, 5)).GetChars(buf);
+            }
+            
+            // replace characters that cannot be saved to ini file
+            for(int i = 0; i< str.Length; i++)
+            {
+                if (str[i] < 32) str[i] = '_';
+            }
+
+            return new SettingsStructFields()
+            {
+                SampleString = new string(str),
+                SampleBool = random.Next(1, 100) < 51,
+                SampleDateTime = new DateTime(DateTime.Now.Ticks / TimeSpan.TicksPerSecond * TimeSpan.TicksPerSecond),
+                SampleTimeSpan = TimeSpan.FromSeconds(random.NextDouble()),
+                SampleDecimal = (decimal)random.NextDouble() / (decimal)random.NextDouble(),
+                SampleDouble = random.NextDouble(),
+                SampleEnum = (SettingEnum)random.Next(0, 9),
+                SampleFlagEnum = (SettingFlagEnum)random.Next(0, 1 << 10),
+                SampleFloat = (float)random.NextDouble(),
+                SampleInt16 = (short)random.Next(short.MinValue, short.MaxValue),
+                SampleInt32 = random.Next() - random.Next(),
+                SampleInt64 = ((long)random.Next() - (long)random.Next()) * (long)random.Next(),
+                SampleInt8 = (sbyte)random.Next(sbyte.MinValue, sbyte.MaxValue),
+                SampleUInt8 = (byte)random.Next(byte.MinValue, byte.MaxValue),
+                SampleUInt16 = (ushort)random.Next(ushort.MinValue, ushort.MaxValue),
+                SampleUInt32 = (uint)random.Next() + (uint)random.Next(),
+                SampleUInt64 = ((ulong)random.Next() + (ulong)random.Next()) * (ulong)random.Next(),
+                SampleNullableUInt32 = random.Next(1, 100) < 20 ? null : (uint?)random.Next(),
+            };
+        }
+    }
+}
